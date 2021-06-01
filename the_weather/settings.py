e70bd560ea722efbe42eee0b2c694bb6659c38dc
Sajini <sajiadl77@gmail.com>
2021-06-01 18:27:30 +0000

    modified

--- the_weather/settings.py
+++ the_weather/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i(tnbko6+y74=6k)&!-3)qsc*m65o%-m+d^8203%a8_^01&mq&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','weather-app-sajini.herokuapp.com']
 
 
 # Application definition
