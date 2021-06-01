e6d44356c3e331cf31cc472f796a9139fafd09ec
Sajini <sajiadl77@gmail.com>
2021-06-01 21:01:50 +0000

    modified

--- the_weather/settings.py
+++ the_weather/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,5 +124,9 @@ STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
